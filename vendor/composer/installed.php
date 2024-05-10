dd06404ab4cb4eb91fecd6e4c5558941c79dc8ad
tangzhouhai <404683342@qq.com>
2024-05-10 11:23:04 +0800

    修改 composer.json格式错误

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'alpaca/base',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4aa3e5283b037c5747ae184f331667e8293da394',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'alpaca/base' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4aa3e5283b037c5747ae184f331667e8293da394',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
